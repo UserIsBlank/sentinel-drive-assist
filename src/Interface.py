cc9779a8a892fcbee4e9452b4dce7f5546c31c62
Kian <92978255+MERL10N@users.noreply.github.com>
2025-12-29 20:28:12 -0800

    Import font for app
    Using roboto slab as proposed

--- src/Interface.py
+++ src/Interface.py
@@ -2,13 +2,18 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.core.window import Window
+from kivy.core.text import LabelBase
+import os
 kivy.require('1.11.1')
+
+font_path = os.path.join(os.path.dirname(__file__), 'fonts')
+LabelBase.register(name="SentinelFont", fn_regular="../fonts/Roboto_Slab/RobotoSlab-VariableFont_wght.ttf")
 
 class Interface(App):
     def build(self):
         Window.clearcolor = (0.2, 0.2, 0.2, 1)
 
-        return Label(text='Sentinel Drive Assist', font_size='24sp')
+        return Label(text='Sentinel Drive Assist', font_name="SentinelFont",font_size='24sp')
 
 
 if __name__ == '__main__':
