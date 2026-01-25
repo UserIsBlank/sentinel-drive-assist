47169d96eae7ec9d555cc33f25a05b8b7a9f1078
Kian <92978255+MERL10N@users.noreply.github.com>
2026-01-24 22:59:58 -0800

    Finalised UI design based on mockups
    Using kivy file to create all UI elements. This is to ensure the python files focus on UI functionality.

--- src/Interface.py
+++ src/Interface.py
@@ -1,45 +1,38 @@
 import kivy
 from kivy.app import App
-from kivy.properties import get_color_from_hex
-from kivy.uix.label import Label
+from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.slider import Slider
-
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ButtonBehavior
 import os
-kivy.require('1.11.1')
 
+kivy.require('1.11.1')
+Window.clearcolor = (0.427, 0.435, 0.478, 1)
+Window.size = (900, 550)
 font_path = os.path.join(os.path.dirname(__file__), 'fonts')
 LabelBase.register(name="SentinelFont", fn_regular="../fonts/Roboto_Slab/RobotoSlab-VariableFont_wght.ttf")
 
-class Interface(App):
-    def build(self):
-        Window.clearcolor = get_color_from_hex("#6D6F7A")
-        layout = FloatLayout()
+# TODO: Add Fail-safe button functionality
+class FailSafeButton(ButtonBehavior, BoxLayout):
+    pass
 
-        # Create Text
-        label = Label(text='Sentinel Drive-Assist', font_name="SentinelFont", font_size='36sp',
-                      pos_hint={'center_x': 0.5, 'center_y': 0.8})
-        layout.add_widget(label)
 
-        # Create failsafe button
-        button1 = Button(text="Fail safe", background_normal='', background_color=get_color_from_hex("7096D1"), size_hint=(None, None), size=(200, 200), pos_hint={"center_x": 0.5, "center_y": 0.5})
-        button1.bind(on_press=lambda _: Window.clearcolor)
-        layout.add_widget(button1)
-
-        # Create slider
-        self.slider = Slider(min = 0, max = 100, value=0, step=1, size_hint=(None, None), size=(500, 100), pos_hint={"center_x": 0.2, "center_y": 0.2})
-        self.slider.bind(value=self.on_slider_value_change)
-        layout.add_widget(self.slider)
-
-        return layout
-
+class Interface(FloatLayout):
+    # TODO: Change the volume based on the slider
     def on_slider_value_change(self, instance, value):
-        print(f"Slider value is: {value}")
+        print(f"Slider value is: {int(value)}")
+
+    # TODO: Add the mute and unmute functionality
+    def on_failsafe_press(self):
+        print("Fail-safe activated!")
+
+class SentinelApp(App):
+    def build(self):
+        Builder.load_file('interface.kv')
+        return Interface()
 
 
 if __name__ == '__main__':
-    Interface().run()
+    SentinelApp().run()
